b6c2715a225f5d0ac561b604c6fce3a7295d587a
TERRY.ROD <tkstorm1988@gmail.com>
2019-09-18 09:37:35 +0800

    add webview support

--- case/webview/webview.go
+++ case/webview/webview.go
@@ -1,5 +1,11 @@
 package main
 
+import "github.com/zserge/webview"
+
 func main() {
-	$END$
+	// Open wikipedia in a 800x600 resizable window
+	//webview.Open("Minimal webview example",
+	//	"https://en.m.wikipedia.org/wiki/Main_Page", 800, 600, true)
+	webview.Open("Minimal webview example",
+		"https://tkstorm.com", 800, 600, true)
 }
